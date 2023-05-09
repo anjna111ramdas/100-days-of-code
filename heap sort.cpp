e4a514e41b9dbcfe6007150ab1b55c134a02867b
Anjna Ramdas <129783639+anjna111ramdas@users.noreply.github.com>
2023-05-09 23:48:23 +0530

    Update heap sort.cpp

--- heap sort.cpp
+++ heap sort.cpp
@@ -35,7 +35,7 @@ void heapSort(int arr[], int n)
     
     for (int i=n-1; i>=0; i--)
     {
-        //Moving current root to end
+        
         swap(arr[0], arr[i]);
  
         //Calling max heapify on the reduced heap
