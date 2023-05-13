f421ce9a400d540ccf8b76f3c07990c838caa0b4
Anjna Ramdas <129783639+anjna111ramdas@users.noreply.github.com>
2023-05-13 12:15:23 +0530

    Update pyramid.cpp

--- pyramid.cpp
+++ pyramid.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-   int i,j,k,space=10; // to print the pyramid in center, you can also increase the # of spaces
+   int i,j,k,space=10; // to print the pyramid in center, you can also increase the # of space
    
 for (int i=0;i<=5;i++)
 {
